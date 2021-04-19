2ed8784c7499da39b200f4decd9abc3868c598a0
Genki Furukawa <genki.furukawa@gmail.com>
2021-04-19 21:11:23 +0900

    [add] ログ出力機能を追加 + flywayの導入

--- src/main/java/com/example/docker/demo/controller/DemoController.java
+++ src/main/java/com/example/docker/demo/controller/DemoController.java
@@ -1,12 +1,15 @@
 package com.example.docker.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class DemoController {
     @GetMapping(value = "/demo")
     public String demo() {
+        log.info("Demo!!");
         return "Demo!!";
     }
 }
